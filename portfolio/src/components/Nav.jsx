2c0a6583b20e42e1db3d37d63b0fc4bf7ea3c821
Dan <dan.edmunds7@gmail.com>
2024-02-09 21:02:35 +0000

    nav icons updated

--- portfolio/src/components/Nav.jsx
+++ portfolio/src/components/Nav.jsx
@@ -1,29 +1,53 @@
 import { Link } from "react-router-dom"
+import { useState } from 'react'
 
 
 export default function Nav() {
+
+    const [hoverHome, setHoverHome] = useState(false)
+    const [hoverMe, setHoverMe] = useState(false)
+    const [hoverContact, setHoverContact] = useState(false)
+
 
     return (
         <div className="nav-container">
             <div className="nav">
                 <div className="nav-items">
                     <Link to='/'>
-                        <div id="nav-home" className="nav-item">
-                            <img className="nav-icon" src="../src/assets/home.png" alt="" />
+                        <div id="nav-home" className="nav-item"
+                            onMouseEnter={() => setHoverHome(true)}
+                            onMouseLeave={() => setHoverHome(false)}>
+                            {hoverHome ? (
+                                <img className="nav-icon" src="../src/assets/home1.png" alt="" />
+                            ) : (
+                                <img className="nav-icon" src="../src/assets/home.png" alt="" />
+                            )}
                             <p>HOME</p>
                         </div>
                     </Link>
 
                     <Link to='/about'>
-                        <div className="nav-item">
-                            <img className="nav-icon" src="../src/assets/me.png" alt="" />
+                        <div className="nav-item" 
+                         onMouseEnter={() => setHoverMe(true)}
+                         onMouseLeave={() => setHoverMe(false)}>
+                        {hoverMe ? (
+                                <img className="nav-icon" src="../src/assets/me1.png" alt="" />
+                            ) : (
+                                <img className="nav-icon" src="../src/assets/me.png" alt="" />
+                            )}
                             <p>ABOUT ME</p>
                         </div>
                     </Link>
 
                     <Link to='/contact'>
-                        <div id="nav-contact" className="nav-item">
-                            <img className="nav-icon" src="../src/assets/mail.png" alt="" />
+                        <div id="nav-contact" className="nav-item" 
+                         onMouseEnter={() => setHoverContact(true)}
+                         onMouseLeave={() => setHoverContact(false)}>
+                        {hoverContact ? (
+                                <img className="nav-icon" src="../src/assets/mail1.png" alt="" />
+                            ) : (
+                                <img className="nav-icon" src="../src/assets/mail.png" alt="" />
+                            )}
                             <p>CONTACT</p>
                         </div>
                     </Link>
