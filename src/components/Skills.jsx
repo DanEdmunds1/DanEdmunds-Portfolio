5a6a6e6381cd047803b431c61a3b657feac73a1d
Dan <dan.edmunds7@gmail.com>
2024-02-13 23:24:14 +0000

    skills stlying added

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -1,26 +1,10 @@
 // Skills
 
 //// Front End
-// JS
-// React
-// HTML5
-// CSS3
-// SASS
 // Bootstrap
-// Python
-
-//// Back End
-// Express.js
-// Django REST Franework
-// Node.js
-
-//// DBs
-// MongoDB
-// PostgreSQL
 
 //// Other
 // Trello
-// VS Code
 // GitHub
 // Insomnia
 import { Container, Row, Col } from "react-bootstrap"
@@ -32,13 +16,32 @@ import react from '../assets/skills/library.png'
 import postgre from '../assets/skills/postgre.png'
 import python from '../assets/skills/python.png'
 import sass from '../assets/skills/sass.png'
-
-
+import node from '../assets/skills/node.png'
+import express from '../assets/skills/express.png'
+import django from '../assets/skills/django.png'
+import mongo from '../assets/skills/mongo.png'
+import vscode from '../assets/skills/vscode.png'
+import github from '../assets/github-con.png'
+import boot from '../assets/skills/boot.png'
 
 
 export default function Skills() {
 
-    const skills = [css, html, js, react, postgre, python, sass
+    const skills = [
+        { name: 'CSS', img: css },
+        { name: 'HTML5', img: html },
+        { name: 'JavaScript', img: js },
+        { name: 'React', img: react },
+        { name: 'PostgreSQL', img: postgre },
+        { name: 'Python', img: python },
+        { name: 'Sass', img: sass },
+        { name: 'Bootstrap', img: boot },
+        { name: 'Node.js', img: node },
+        { name: 'Express.js', img: express },
+        { name: 'Django', img: django },
+        { name: 'MongoDB', img: mongo },
+        { name: 'VSCode', img: vscode },
+        { name: 'GitHub', img: github },
     ]
 
     return (
@@ -48,13 +51,17 @@ export default function Skills() {
             <Container fluid className="skills-grid">
                 <Row className="skills-list">
                     {skills.map((skill, i) => {
+                        console.log(skill)
                         return (
-                            <Col className="skill" 
-                            key={i} 
-                            xs={6} 
-                            md={4} 
-                            lg={3}>
-                                <img key={i} src={skill} alt="skill" />
+                            <Col className="skill"
+                                key={i}
+                                xs={6}
+                                md={4}
+                                lg={3}>
+                                <div className="image-container">
+                                    <img key={i} src={skill.img} alt="skill" />
+                                    <p>{skill.name}</p>
+                                </div>
                             </Col>
                         )
                     })}
