7eff60f28296a332ea2e89a2dbf90cfa264ca417
Dan <dan.edmunds7@gmail.com>
2024-11-23 14:42:35 +0000

    added campaign conductor info

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -23,21 +23,28 @@ import mongo from '../assets/skills/mongo.png'
 import vscode from '../assets/skills/vscode.png'
 import github from '../assets/github-con.png'
 import boot from '../assets/skills/boot.png'
+import angular from '../assets/skills/angular.png'
+import typescript from '../assets/skills/typescript.png'
+import sanity from '../assets/skills/sanity.png'
 
 
 export default function Skills() {
 
     const skills = [
-        { name: 'CSS', img: css },
+  
         { name: 'HTML5', img: html },
         { name: 'JavaScript', img: js },
-        { name: 'React', img: react },
-        { name: 'PostgreSQL', img: postgre },
+        { name: 'TypeScript', img: typescript },
         { name: 'Python', img: python },
+        { name: 'React', img: react },
+        { name: 'Angular', img: angular },
+        { name: 'PostgreSQL', img: postgre },
+        { name: 'CSS', img: css },
         { name: 'Sass', img: sass },
         { name: 'Bootstrap', img: boot },
         { name: 'Node.js', img: node },
         { name: 'Express.js', img: express },
+        { name: 'Sanity', img: sanity },
         { name: 'Django', img: django },
         { name: 'MongoDB', img: mongo },
         { name: 'VSCode', img: vscode },
