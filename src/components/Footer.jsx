fd66cf2d15f4e8490eaeb9775661745c0d4c287c
Dan <dan.edmunds7@gmail.com>
2026-02-06 15:57:05 +0000

    fixed linkedin url

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -6,7 +6,7 @@ export default function Footer() {
     return (
         <footer>Dan Edmunds {currentYear}
                 <a href="https://github.com/danedmunds1" target="_blank" rel="noopener noreferrer"><img src={githubIcon} className="footer-icon" alt="GitHub Link" /></a>
-                <a href="https://www.linkedin.com/in/daniel-edmunds7/" target="_blank" rel="noopener noreferrer"><img src={linkedinIcon} className="footer-icon" alt="Linkedin Link" /></a>
+                <a href="https://www.linkedin.com/in/dan-edmunds/" target="_blank" rel="noopener noreferrer"><img src={linkedinIcon} className="footer-icon" alt="Linkedin Link" /></a>
         </footer>
     )
 }
